fdee302f3af52e8daf783c024c3caf1fbad736b1
Raulnet <hormm@hotmail.fr>
2014-08-03 17:47:54 +0200

    Sonic Javascript Paralax
    Sonic Javascript Paralax

---
+++ index.php
@@ -0,0 +1,18 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <meta http-equiv="content-type" content="text/html" charset="UTF-8"/>
+    <meta name="description" content="un sonic qui cour !!!" type="text/css"/>
+    <link href="css/reset.css" rel="stylesheet" type="text/css">
+    <link href="css/style.css" rel="stylesheet" type="text/css">
+    <title>Sonic</title>
+</head>
+
+<body>
+
+
+<script src="js/script.js"></script>
+
+
+</body>
+</html>
